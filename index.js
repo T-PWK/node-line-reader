6f6a69e0242b2edd32343c756f808be68b1b328c
Tom <pawlaktom@gmail.com>
2014-11-25 13:06:27 +0000

    Code formatting updated; jshint issues corrected

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+(function () {
+	"use strict";
 
-module.exports.LineTransform = require('./lib/transformer');
-module.exports.LineReader    = require('./lib/reader');
-module.exports.LineFilter    = require('./lib/filter');
+	module.exports.LineTransform = require('./lib/transformer');
+	module.exports.LineReader    = require('./lib/reader');
+	module.exports.LineFilter    = require('./lib/filter');
+}());
